de21e103499d322db78e7b75a7f1436764ed7b7a
zhangkui <1627528424@qq.com>
2019-09-06 10:14:51 +0800

    add ewt

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,30 +1,37 @@
 import React, { useState, useEffect, useRef } from 'react'
 
-function extends(child, parent) {
-    const prototype = Object.create(parent.prototype)
-    prototype.constructor = child
-    child.prototype = prototype
+// function extends(child, parent) {
+//     const prototype = Object.create(parent.prototype)
+//     prototype.constructor = child
+//     child.prototype = prototype
+// }
+const countRef = {
+    current: null,
 }
 
 function Counter() {
     const [count, setCount] = useState(0);
-    const countRef = useRef(null)
+    //const countRef = useRef(null)
 
-    function callback() {
-        setCount(count+1)
-    }
+    // function callback() {
+    //     setCount(count+1)
+    // }
 
     useEffect(() => {
-        countRef.current = callback
+        countRef.current = count
+        var times = countRef.current
+        setTimeout(() => {
+            console.log(`You clicked ${times} times`);
+          }, 3000);
     })
 
-    useEffect(() => {
-        function tick() {
-            countRef.current()
-        }
-        const id = setInterval(tick, 1000)
-        return () => clearInterval(id)
-    }, [])
+    // useEffect(() => {
+    //     function tick() {
+    //         countRef.current()
+    //     }
+    //     const id = setInterval(tick, 1000)
+    //     return () => clearInterval(id)
+    // }, [])
 
     return (
         <div>
